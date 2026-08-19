7b83ad8b7ab177a69a75058aa0e59413f69ee600
SinTan1729 <sayantan.santra689@gmail.com>
2026-08-19 17:14:46 -0500

    chg: Use less js

--- static/script.js
+++ static/script.js
@@ -8,31 +8,8 @@ const lastModifiedTime = () => {
   last_modified_span.innerHTML = last_modified_time.toDateString();
 };
 
-// Open all links in new tab
-const openInNewTab = () => {
-  document.querySelectorAll("a").forEach((url) => {
-    url.target = "_blank";
-    url.rel = "noreferrer noopener";
-  });
-};
-
-// Only open one detail element at one time
-const onlyOpenOneDetail = () => {
-  const detailsList = document.querySelectorAll("details");
-  function handleDetailToggle(event) {
-    if (event.newState == "closed") return;
-    for (let detail of detailsList) {
-      detail.open = detail == event.target;
-    }
-  }
-  for (let details of detailsList) {
-    details.ontoggle = handleDetailToggle;
-  }
-};
-
 // Start loading here
 lastModifiedTime();
 openInNewTab();
-onlyOpenOneDetail();
 // This uses KaTeX script loaded earlier
 renderMathInElement(document.body);
